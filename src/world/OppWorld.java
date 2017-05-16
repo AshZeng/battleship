0b2bc9fec3fb760b7f623407066096dc8ed4c34d
Cameron <s3589163@student.rmit.edu.au>
2017-05-16 23:01:44 +1000

    Updated OppWorld to printout world to console for easier debugging

--- src/world/OppWorld.java
+++ src/world/OppWorld.java
@@ -10,8 +10,9 @@ import world.World.Coordinate;
 public class OppWorld {
 	
 	private World world = new World();
+	private boolean checkPossibles = false;
 	
-	enum cellState { Untested, Possible, Hit, Miss }; 
+	public enum cellState { Untested, Possible, Hit, Miss }; 
 	/* 
 	 * NOTE although there is a cell state for Possible, this is used to simplify
 	 *   the checking process. The list below is to allow quick access without 
@@ -27,9 +28,13 @@ public class OppWorld {
 	private int numColumns;
 	
 	// Constructor Class
-	public OppWorld( int numRow, int numColumn )
+	public OppWorld( int numRow, int numColumn, boolean checkPossibles )
 	{
 		oppWorld = new cellState[numRow][numColumn];
+		
+		this.numRows = numRow;
+		this.numColumns = numColumn;
+		this.checkPossibles = checkPossibles;
 		
 		// Initialize ALL cells to untested
 		for ( int j = 0; j < numRows; j++ )
@@ -46,23 +51,25 @@ public class OppWorld {
 		
 		// If a hit was detected, we need to update the surrounding cells to 
 		//   check for possible ships
-		if ( state == cellState.Hit )
-			calculatePossibles( row, column );	
+		if ( ( state == cellState.Hit ) && ( this.checkPossibles ) )
+		{
+			calculatePossibles( row, column );
+		}
+		printWorld( );
 	}
 	
 	// Checks the state of the cells above, below, left and right of the target cell
 	private void calculatePossibles( int row, int column )
 	{
 		int temp;
-		
 		// Check Up
-		temp = row - 1;
-		if ( temp >= 0 )
+		temp = row + 1;
+		if ( temp < numRows )
 			checkCellUntested( temp, column );
 		
 		// Check Down
-		temp = row + 1;
-		if ( temp < numRows )
+		temp = row - 1;
+		if ( temp >= 0 )
 			checkCellUntested( temp, column );
 		
 		// Check Right
@@ -71,27 +78,71 @@ public class OppWorld {
 			checkCellUntested( row, temp );
 		
 		// Check Left
-		temp = column + 1;
+		temp = column - 1;
 		if ( temp >= 0 )
 			checkCellUntested( row, temp );	
 	}
 	
 	private void checkCellUntested( int row, int column )
 	{
-		switch( oppWorld[row][column] )
+		// If the cell is untested, it needs to become a "possible"
+		if ( oppWorld[row][column] == cellState.Untested )
 		{
-			// If the cell is untested, it needs to become a "possible"
-			case Untested:
-			{
-				Coordinate temp = world.new Coordinate();
-				temp.row = row;
-				temp.column = column;
-				
-				possibleTargets.add(temp);
-			}
-			// If the cell is already marked as hit or miss or possible, then it is ignored
-			default: 
-				return;
+			oppWorld[row][column] = cellState.Possible;
+			Coordinate tempCoord = world.new Coordinate();
+			tempCoord.row = row;
+			tempCoord.column = column;
+			
+			possibleTargets.add(tempCoord);
 		}
+		// If the cell is already marked as hit or miss or possible, then it is ignored
 	}
+	
+	
+    public void printWorld( )
+    {
+    	char letter;
+    	
+    	printLine();
+    	
+    	
+//		for ( int j = 0; j < numRows; j++ )
+    	for ( int j = numRows - 1; j >= 0 ; j-- )
+		{
+			System.out.print( j + "|");
+			
+			for ( int i = 0; i < numColumns; i++ )
+			{
+				if ( oppWorld[j][i] == cellState.Untested )
+					letter = ' ';
+				else if ( oppWorld[j][i] == cellState.Hit )
+					letter = 'H';
+				else if ( oppWorld[j][i] == cellState.Miss )	
+					letter = 'M';
+				else if ( oppWorld[j][i] == cellState.Possible )
+					letter = 'P';
+				else
+					letter = 'X';
+				System.out.print( letter + "|");
+			}
+			System.out.print("\n");
+		}
+		printHeader();
+    }
+
+    public void printHeader()
+    {
+    	System.out.print(" ");
+    	for ( int i = 0; i < numColumns; i++ )
+		   	System.out.print(" " + i );
+    	System.out.print("\n");
+    }
+    
+    public void printLine()
+    {
+    	System.out.print("\n");
+    	for ( int i = 0; i < numColumns + 1; i++ )
+		   	System.out.print("--");
+    	System.out.print("\n");
+    }
 }
