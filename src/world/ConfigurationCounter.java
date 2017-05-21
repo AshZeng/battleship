8c2b8f322ed1bc5e4dc3d510c0ab9c09dcb35f3f
Cameron <s3589163@student.rmit.edu.au>
2017-05-22 01:07:38 +1000

    Updates to ConfigCounter to support Cams Monte Player

--- src/world/ConfigurationCounter.java
+++ src/world/ConfigurationCounter.java
@@ -5,7 +5,7 @@ package world;
  * that pass through a single coordinate on the board.
  * Numbers used for edge cases:
  * -1 Represents a shot has been fired on that coordinate
- *  0 Represents no possible ship combinations for that particular coordinate 
+ * ????? Represents no possible ship combinations for that particular coordinate 
  * @author patstockwell
  *
  */
@@ -21,6 +21,67 @@ public class ConfigurationCounter {
 		this.shipSize = shipSize;
 		this.columns = columns;
 		this.rows = rows;
+		initializeArrayToZero();
 	}
 
+	private void initializeArrayToZero(){
+		for(int y = 0; y < rows; ++y){ // for each row
+			for(int x = 0; x < columns; ++x){ // for each column
+				ShipConfigurationCounts[y][x] = 0;
+			}
+		}
+	}
+	
+	// Reset all possibilities (except shots made) to 0
+	public void resetPossibilities( ) {
+		int value;
+		for(int y = 0; y < rows; ++y){ // for each row
+			for(int x = 0; x < columns; ++x){ // for each column
+				value = ShipConfigurationCounts[y][x];
+				// Will Ignore all shots marked as well as areas that 
+				// have previously been declared not possible (ie 0)
+				if ( value > 0  )
+					ShipConfigurationCounts[y][x] = 0;
+			}
+		}
+	}
+	
+	// Print the current configuration counter to the console
+    public void printConfigCounter( )
+    { 	
+    	int currentValue;
+    	printLine();
+    	for ( int j = rows - 1; j >= 0 ; j-- )
+		{
+			System.out.print( j + "|");
+			
+			for ( int i = 0; i < columns; i++ )
+			{
+				currentValue = ShipConfigurationCounts[j][i];
+				
+				if ( (currentValue > 9) || (currentValue < 0) )
+					System.out.print( currentValue + "|");
+				else 
+					System.out.print( " " + currentValue + "|");
+			}
+			System.out.print("\n");
+		}
+		printHeader();
+    }
+
+    public void printHeader()
+    {
+    	System.out.print(" ");
+    	for ( int i = 0; i < columns; i++ )
+		   	System.out.print("  " + i );
+    	System.out.print("\n");
+    }
+    
+    public void printLine()
+    {
+    	System.out.print("\n");
+    	for ( int i = 0; i < columns + 1; i++ )
+		   	System.out.print("---");
+    	System.out.print("\n");
+    }
 }
